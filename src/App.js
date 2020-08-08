8f119c1eee3a0153866f7e6d23bb8ff708733d54
Trish Ness <trishnss@gmail.com>
2020-08-07 18:33:20 -0700

    bug fixed, info displaying

--- src/App.js
+++ src/App.js
@@ -1,88 +1,74 @@
 import React, { Component } from 'react';
-import axios from "axios";
+import axios from "axios"
 import './App.css';
-
-function EmployeeCard(img, name, phone){
+function EmployeeCard({ img, name, phone }) {
   return (
-  <div>
-    <img src={img} alt={name.first}/>
-    <div>
-      <p>{`${name.title} ${name.first} ${name.last}`}</p>
-      <p>{phone}</p>
-    </div>
-  </div>
+    <>
+      <div>
+        <img src={img} alt={name.first} />
+        <div>
+          <p>{`${name.title} ${name.first} ${name.last}`}</p>
+          <p>{phone}</p>
+        </div>
+      </div>
+    </>
   )
 }
-
 const styles={
-  empCOntainer:{
+  employeeContainer:{
     display:"flex",
-    flexWrap:"wrap",
-    justifyContent:"center"
+    flexWrap: "wrap",
+    justifyContent: "center"
   }
 }
-
 class App extends Component {
-
   state = {
-    users:[],
-    numInput: 0
+    numInput: 0,
+    users: []
   }
-
-  handleInputChange = event => {
-    const {name, value} = event.target
-    this.setState({
-      [name]:value
-    })
+  handleInputChange = e => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
   }
-
-  makeRequest = async ()=>{
-    const URL = `https://randomuser.me/api/?results=${this.state.numInput}nat=us`;
-
-    try{
-      let results = await axios.get(URL);
-      console.log(results.data.results);
-      this.setState({ users:results.data.results });
+  makeRequest = async () => {
+    const URL = `https://randomuser.me/api/?results=${this.state.numInput}&nat=us`
+    try {
+      let results = await axios.get(URL)
+      this.setState({ users: results.data.results })
       console.log(results)
-
-    } catch(error){
-      console.log("error: ", error)
+    } catch (e) {
+      console.log("Error:", e)
     }
   }
-
   renderEmployees = () => {
-    return this.state.users.map((user)=>(
-    <EmployeeCard 
-    key={user.id.value} 
-    img={user.picture.large} 
-    name={user.name} 
-    phone={user.phone}
-    />
-  ));
-  };
-
+    return this.state.users.map(user => <EmployeeCard
+      key={user.id.value}
+      img={user.picture.large}
+      name={user.name}
+      phone={user.phone}
+    />);
+  }
   render() {
+    const isNumberEntered = this.state.numInput === 0
     return (
       <div className="App">
-        <h1>Employee Directory</h1>
-        <label htmlFor="numInput">
-          # of Employees
-        <input 
-          id="numInput" 
-          name="numInput" 
-          type="number" 
-          value={this.state.numInput} 
-          min="0" 
-          onChange={this.handleInputChange}/>
+        <h1>Fire&Vine Hospitality</h1>
+        <label htmlFor="numInput"># of Employees
+      <input
+            id="numInput"
+            name="numInput"
+            type="number"
+            value={this.state.numInput}
+            min="0"
+            onChange={this.handleInputChange}
+          />
         </label>
-        <button disabled={this.state.numInput === 0} onClick={this.makeRequest}>Submit</button>
-
-        <div style={styles.empContainer}>
+        <button disabled={isNumberEntered} onClick={this.makeRequest} className="btn btn-primary">{isNumberEntered ? "Please Enter a Number" : "Submit"}</button>
+        <div style={styles.employeeContainer}>
           {this.renderEmployees()}
         </div>
       </div>
     );
   }
 }
-
 export default App;

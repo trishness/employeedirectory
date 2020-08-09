03677a630792db666b16ed8e928e87bf0ddc5ec9
Trish Ness <trishnss@gmail.com>
2020-08-09 11:59:27 -0700

    filter and sort added, button functionality not registering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import axios from "axios"
 import './App.css';
-function EmployeeCard({ img, name, phone }) {
+
+function EmployeeCard({ img, name, phone, email }) {
   return (
     <>
       <div>
         <img src={img} alt={name.first} />
         <div>
-          <p>{`${name.title} ${name.first} ${name.last}`}</p>
+          <p>{`${name.first} ${name.last}`}</p>
           <p>{phone}</p>
+          <p>{email}</p>
         </div>
       </div>
     </>
@@ -21,15 +23,20 @@ const styles={
     justifyContent: "center"
   }
 }
+
 class App extends Component {
+
   state = {
     numInput: 0,
-    users: []
+    users: [],
+    filteredUsers: []
   }
+
   handleInputChange = e => {
     const { name, value } = e.target
     this.setState({ [name]: value })
   }
+
   makeRequest = async () => {
     const URL = `https://randomuser.me/api/?results=${this.state.numInput}&nat=us`
     try {
@@ -40,14 +47,41 @@ class App extends Component {
       console.log("Error:", e)
     }
   }
+
+  filterFemaleEmployees = () => {
+    const usersCopy = [...this.state.users]
+    const filteredUsers = usersCopy.filter((user) => user.gender==="female")
+    this.setState({filteredUsers:filteredUsers})
+  }
+
+  filterMaleEmployees = () => {
+    const usersCopy = [...this.state.users]
+    const filteredUsers = usersCopy.filter((user) => user.gender==="male")
+    this.setState({filteredUsers:filteredUsers})
+  }
+
+  sortABC = () => {
+    const usersCopy = [...this.state.users]
+    const sortedUsers = usersCopy.sort((a,b) => {
+      if (a.name.last < b.name.last) {
+        return -1
+      } else {
+        return 1
+      }
+    })
+    this.setState({filteredUsers:sortedUsers})
+  }
+
   renderEmployees = () => {
     return this.state.users.map(user => <EmployeeCard
       key={user.id.value}
       img={user.picture.large}
       name={user.name}
       phone={user.phone}
+      email={user.email}
     />);
   }
+
   render() {
     const isNumberEntered = this.state.numInput === 0
     return (
@@ -64,6 +98,11 @@ class App extends Component {
           />
         </label>
         <button disabled={isNumberEntered} onClick={this.makeRequest} className="btn btn-primary">{isNumberEntered ? "Please Enter a Number" : "Submit"}</button>
+        <br></br>
+        <button onClick={this.filterFemaleEmployees} className="btn btn-primary">Filter by Females</button>
+        <button onClick={this.filterMaleEmployees} className="btn btn-primary">Filter by Males</button>
+        <br></br>
+        <button onClick={this.sortABC} className="btn btn-primary">Sort Alphabetically</button>
         <div style={styles.employeeContainer}>
           {this.renderEmployees()}
         </div>
